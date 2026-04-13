fe8ca45921f6c20ca9b92d85b14b37d16215fc15
Anushka Rakesh <anushkarakesh111@gmail.com>
2026-04-13 10:50:08 +0530

    Fix daily_events model kind syntax

--- models/sales/daily_events.sql
+++ models/sales/daily_events.sql
@@ -1,14 +1,10 @@
 MODEL (
   name ECOMMERCE_PLATFORM.sales.daily_events,
-  kind FULL (
-    time_column event_date,
-    batch_size 1
-  ),
-  start '2025-01-01',
-  cron '@daily',
+  kind FULL,
   grains (event_date),
   description 'Daily event aggregation'
 );
+
 
 SELECT
   o_orderdate AS event_date,
